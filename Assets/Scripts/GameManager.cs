f7ad9e53019b2d51939ff4979fcc650a48284873
NicholasA2 <nadiohos@icloud.com>
2023-10-19 21:27:00 -0400

    Ammo and health pickups complete, healing too

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    public int healingPotions = 1;
+    public int ammo = 5;
+
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void AddHealingToInventory()
+    {
+        healingPotions++;
+    }
+
+    public void AddAmmoToInventory()
+    {
+        ammo += 5;
+    }
+}
