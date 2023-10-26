c952e20012dd3fde9ef5793040a0f0308cef1681
George Athanasatos <agentsolo13@gmail.com>
2023-10-26 12:10:54 -0400

    fixed crossbow and bolt
    just need to add the scrip to the game object

--- Assets/Scripts/CrossbowBolt.cs
+++ Assets/Scripts/CrossbowBolt.cs
@@ -2,35 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CrossbowBolts : MonoBehaviour
+public class CrossbowBolt : MonoBehaviour
 {
-    public float speed = 200;
-    Vector3 lastPosition;
-    RaycastHit[] hitEnemy;
-    public GameObject hit;
+    public int damage = 10; // Damage inflicted by the bolt
+    public float lifeDuration = 5.0f; // Time in seconds before the bolt disappears
 
-    void Start()
+    private void Start()
     {
-        Destroy(gameObject, 1);
-       // hit = new RaycastHit[1];
+        // Set a timer to destroy the bolt after a certain duration
+        Destroy(gameObject, lifeDuration);
     }
 
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        lastPosition = transform.position;
-        transform.position += transform.forward * speed * Time.deltaTime;
-        CheckHit();
-    }
+        // Handle collision with other objects
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            // Check if the collided object has an "Enemy" tag (you can adjust this tag)
+            EnemyHealth enemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
 
-    void CheckHit()
-    {
-        Ray ray = new Ray(lastPosition, transform.forward);
-        float dist = Vector3.Distance(lastPosition, transform.forward);
-       // if(Physics.RaycastNonAlloc(ray, hit, dist) > 0)
-        //{
-        //    Instantiate(hitEnemy, hit[0].point, Quaternion.LookRotation(hit[0].normal));
-        //    Destroy(gameObject);
-        //}
+            if (enemyHealth != null)
+            {
+                // If the collided object has an EnemyHealth component, deal damage to it
+                enemyHealth.TakeDamage(damage);
+            }
+
+            // Destroy the bolt on impact
+            Destroy(gameObject);
+        }
+        else
+        {
+            // Destroy the bolt on any other collision
+            Destroy(gameObject);
+        }
     }
-    
 }
--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,18 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    private void Start()
     {
-        
+        currentHealth = maxHealth;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TakeDamage(int damage)
     {
-        
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        // Handle enemy death, e.g., play death animation, remove from the scene, etc.
+        Destroy(gameObject);
     }
 }
